706b5b41b0f5fa32f7f4ff2b0b8c8b1d9851bb7c
Benjamin Futász <34510704+bfut@users.noreply.github.com>
2019-07-20 10:45:30 +0200

    Add files via upload

--- Items Editing/bfut_Paste item from clipboard to selected items (replace).lua
+++ Items Editing/bfut_Paste item from clipboard to selected items (replace).lua
@@ -1,13 +1,13 @@
 --[[
   @author bfut
-  @version 1.0
+  @version 1.1
   DESCRIPTION: bfut_Paste item from clipboard to selected items (replace)
   HOW TO USE:
     1) Select media item.
     2) Run script "bfut_Copy item to clipboard".
     3) Select other media item(s).
     4) Run script "bfut_Paste item from clipboard to selected items (replace)".
-  REQUIRES: Reaper v5.95 or later, SWS v2.9.7 or later
+  REQUIRES: Reaper v5.980 or later, SWS v2.10.0.1 or later
   LICENSE:
     Copyright (c) 2018 and later Benjamin Futasz <bendfu@gmail.com><https://github.com/bfut>
     
@@ -39,11 +39,12 @@ local config = {
 }
 local reaper = reaper
 local type = type
+local ipairs = ipairs
 local function bfut_GetSetItemChunkValue2(s,key,param,set)
-  if type(s) ~= "string" then reaper.ReaScriptError("bfut_GetSetItemChunkValue: string expected (arg 1)") return s,false end
-  if type(key) ~= "string" then reaper.ReaScriptError("bfut_GetSetItemChunkValue: string expected (arg 2)") return s,false end
-  if type(param) ~= "string" and type(param) ~= "number" then reaper.ReaScriptError("bfut_GetSetItemChunkValue: string expected (arg 3)") return false,s end
-  if type(set) ~= "boolean" then reaper.ReaScriptError("bfut_GetSetItemChunkValue: boolean expected (arg 4)") return s,false end
+  if type(s) ~= "string" then reaper.ReaScriptError("bfut_GetSetItemChunkValue2: string expected (arg 1)") return s,false end
+  if type(key) ~= "string" then reaper.ReaScriptError("bfut_GetSetItemChunkValue2: string expected (arg 2)") return s,false end
+  if type(param) ~= "string" and type(param) ~= "number" then reaper.ReaScriptError("bfut_GetSetItemChunkValue2: string expected (arg 3)") return false,s end
+  if type(set) ~= "boolean" then reaper.ReaScriptError("bfut_GetSetItemChunkValue2: boolean expected (arg 4)") return s,false end
   if set then
     if s:match('('..key..')') ~= nil then
       return s:gsub('%s'..key..'%s+.-[\r]-[%\n]',"\n"..key.." "..param.."\n",1),true
@@ -54,9 +55,17 @@ local function bfut_GetSetItemChunkValue2(s,key,param,set)
     if s:match('('..key..')') ~= nil then
       return s:match('%s'..key..'%s+(.-)[\r]-[%\n]'),true
     else
-      return nil,true
+      return nil,false
     end
   end
+end
+local function bfut_ResetAllChunkGuids(s,key)
+  if type(s) ~= "string" then reaper.ReaScriptError("bfut_ResetAllChunkGuids: string expected (arg 1)") return s,false end
+  if type(key) ~= "string" or type(key) == "number" then reaper.ReaScriptError("bfut_ResetAllChunkGuids: string expected (arg 2)") return s,false end
+  while s:match('%s('..key..')') ~= nil do
+    s = s:gsub('%s('..key..')%s+.-[\r]-[%\n]',"\ntemp%1 "..reaper.genGuid("").."\n",1)
+  end
+  return s:gsub('temp'..key,key),true
 end
 local RPPXML_LOCK = {
   ["1"] = 0,
@@ -71,26 +80,22 @@ if type(config["pool_midi"]) ~= "boolean" then
   reaper.ReaScriptError("config[pool_midi] must be Boolean.\n")
   return
 end
-if not reaper.APIExists("SNM_CreateFastString") then
-  reaper.ReaScriptError("Requires extension, SWS v2.9.7 or later.\n")
-  return
-end
-if not reaper.APIExists("SNM_DeleteFastString") then
-  reaper.ReaScriptError("Requires extension, SWS v2.9.7 or later.\n")
-  return
-end
-if not reaper.APIExists("CF_GetClipboardBig") then
-  reaper.ReaScriptError("Requires extension, SWS v2.9.7 or later.\n")
-  return
+for _,function_name in ipairs({"SNM_CreateFastString",
+                               "SNM_DeleteFastString",
+                               "CF_GetClipboardBig"}) do
+  if not reaper.APIExists(function_name) then
+    reaper.ReaScriptError("Requires extension, SWS v2.10.0.1 or later.\n")
+    return
+  end
 end
 local WDL_FastString = reaper.SNM_CreateFastString("")
-local strFromClipboard = reaper.CF_GetClipboardBig(WDL_FastString)
+local buf = reaper.CF_GetClipboardBig(WDL_FastString)
 reaper.SNM_DeleteFastString(WDL_FastString)
 WDL_FastString = nil
-if strFromClipboard == nil then
+if buf == nil then
   return
 end
-if strFromClipboard:find('<ITEM',0,true) ~= 1 then
+if buf:find('<ITEM',0,true) ~= 1 then
   return
 end
 reaper.Undo_BeginBlock2(0)
@@ -102,29 +107,26 @@ for i=0,gl_CountSelectedMediaItems-1 do
     if locked == 0.0 or locked == 2.0 then
       local retval,item_chunk = reaper.GetItemStateChunk(item,"",false)
       if retval and item_chunk ~= nil then
-        local item_position = bfut_GetSetItemChunkValue2(item_chunk,"POSITION","",false)
-        local item_length = bfut_GetSetItemChunkValue2(item_chunk,"LENGTH","",false)
-        local item_mute = bfut_GetSetItemChunkValue2(item_chunk,"MUTE","",false)
-        local item_iid = bfut_GetSetItemChunkValue2(item_chunk,"IID","",false)
-        local s = strFromClipboard
-        s = bfut_GetSetItemChunkValue2(s,"POSITION",item_position,true)
-        s = bfut_GetSetItemChunkValue2(s,"LENGTH",item_length,true)
-        s = bfut_GetSetItemChunkValue2(s,"MUTE",item_mute,true)
-        s = bfut_GetSetItemChunkValue2(s,"SEL",1,true)
-        s = bfut_GetSetItemChunkValue2(s,"IID",item_iid,true)
-        s = bfut_GetSetItemChunkValue2(s,"LOCK",RPPXML_LOCK[bfut_GetSetItemChunkValue2(s,"LOCK","",false) or "1"],true)
-        s = bfut_GetSetItemChunkValue2(s,"IGUID",reaper.genGuid(""),true)
-        while s:match('%s(GUID)') ~= nil do
-          s = s:gsub('%s(GUID)%s+.-[\r]-[%\n]',"\ntemp%1 "..reaper.genGuid("").."\n",1)
+        local src_item_chunk = buf
+        for _,key in ipairs({"POSITION","LENGTH","MUTE","SEL","IID"}) do
+          src_item_chunk = bfut_GetSetItemChunkValue2(
+            src_item_chunk,
+            key,
+            bfut_GetSetItemChunkValue2(item_chunk,key,"",false),
+            true
+          )
         end
-        s = s:gsub('temp'.."GUID","GUID")
+        src_item_chunk = bfut_GetSetItemChunkValue2(
+          src_item_chunk,"LOCK",
+          RPPXML_LOCK[bfut_GetSetItemChunkValue2(src_item_chunk,"LOCK","",false) or "1"],
+          true
+        )
+        src_item_chunk = bfut_GetSetItemChunkValue2(src_item_chunk,"IGUID",reaper.genGuid(""),true)
+        src_item_chunk = bfut_ResetAllChunkGuids(src_item_chunk,"GUID")
         if not config["pool_midi"] then
-          while s:match('%s(POOLEDEVTS)') ~= nil do
-            s = s:gsub('%s(POOLEDEVTS)%s+.-[\r]-[%\n]',"\ntemp%1 "..reaper.genGuid("").."\n",1)
-          end
-          s = s:gsub('temp'.."POOLEDEVTS","POOLEDEVTS")
+          src_item_chunk = bfut_ResetAllChunkGuids(src_item_chunk,"POOLEDEVTS")
         end
-        if not reaper.SetItemStateChunk(item,s,true) then
+        if not reaper.SetItemStateChunk(item,src_item_chunk,true) then
           reaper.SetItemStateChunk(item,item_chunk,false)
         end
       end
