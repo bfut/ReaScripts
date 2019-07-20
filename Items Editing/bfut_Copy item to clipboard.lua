df8533ca75987f5b94120cf77eb1d9f7c833421c
Benjamin Futász <34510704+bfut@users.noreply.github.com>
2019-07-20 10:45:16 +0200

    Add files via upload

--- Items Editing/bfut_Copy item to clipboard.lua
+++ Items Editing/bfut_Copy item to clipboard.lua
@@ -1,33 +1,14 @@
 --[[
   @author bfut
-  @version 1.0
+  @version 1.1
   DESCRIPTION: bfut_Copy item to clipboard
   HOW TO USE:
     1) Select media item.
     2) Run script "bfut_Copy item to clipboard".
     3) Select other media item(s).
     4) Run script "bfut_Paste item from clipboard to selected items (replace)".
-  REQUIRES: Reaper v5.95 or later, SWS v2.9.7 or later
-  LICENSE:
-    Copyright (c) 2018 and later Benjamin Futasz <bendfu@gmail.com><https://github.com/bfut>
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+  REQUIRES: Reaper v5.980 or later, SWS v2.10.0.1 or later
+  LICENSE: Public Domain
 ]]
 local reaper = reaper
 if reaper.CountSelectedMediaItems(0) < 1 then
@@ -35,7 +16,7 @@ if reaper.CountSelectedMediaItems(0) < 1 then
 end
 local function_name ="CF_SetClipboard"
 if not reaper.APIExists(function_name) then
-  reaper.ReaScriptError("Requires extension, SWS v2.9.7 or later.\n")
+  reaper.ReaScriptError("Requires extension, SWS v2.10.0.1 or later.\n")
   return
 end
 local _,item_chunk = reaper.GetItemStateChunk(reaper.GetSelectedMediaItem(0,0),"",false)
