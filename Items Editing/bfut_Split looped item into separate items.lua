7c0aa9392415db6f8a12e8b1aa7990da61af697e
bfut <34510704+bfut@users.noreply.github.com>
2020-02-22 13:49:01 +0100

    Change: bfut_Split looped item into separate items.lua

--- Items Editing/bfut_Split looped item into separate items.lua
+++ Items Editing/bfut_Split looped item into separate items.lua
@@ -1,53 +1,66 @@
 --[[
   @author bfut
-  @version 1.0
-  DESCRIPTION: bfut_Split looped item into separate items
-  HOW TO USE:
-    1) Select media item(s).
-    2) Run the script.
-  REQUIRES: Reaper v5.70 or later
+  @version 1.1
+  @description bfut_Split looped item into separate items
+  @about
+    HOW TO USE:
+      1) Select media item(s).
+      2) Run the script.
+    REQUIRES: Reaper v5.99 or later
+  @changelog
+    + ignore locked items
+  @website https://github.com/bfut
   LICENSE:
-    Copyright (c) 2017 and later Benjamin Futasz <bendfu@gmail.com><https://github.com/bfut>
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+    Copyright (C) 2017 and later Benjamin Futasz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
-local r = reaper
-local c = r.CountSelectedMediaItems(0)
-if c < 1 then return end
-r.PreventUIRefresh(1)
-r.Undo_BeginBlock2(0)
-local function bfut_SplitLpdItmsIntoSeparateItms(item)
-  r.SelectAllMediaItems(0,false)
-  r.SetMediaItemSelected(item,true)
-  repeat
-    local tk = r.GetActiveTake(item,0)
-    if tk ~= nil then
-      local l, v = r.GetMediaSourceLength(r.GetMediaItemTake_Source(tk))
-      if v then l = r.TimeMap_QNToTime(l) end
-      item = r.SplitMediaItem(item,r.GetMediaItemInfo_Value(item,"D_POSITION")+l/r.GetMediaItemTakeInfo_Value(tk,"D_PLAYRATE"))
-    end
-  until not item and true
+local COUNT_SEL_ITEMS = reaper.CountSelectedMediaItems(0)
+if COUNT_SEL_ITEMS < 1 then
   return
 end
+local function bfut_SplitLoopedItemAtLoopPoints(item)
+  reaper.SelectAllMediaItems(0, false)
+  reaper.SetMediaItemSelected(item, true)
+  repeat
+    local item_activetake = reaper.GetActiveTake(item, 0)
+    if item_activetake then
+      local item_activetake_mediasourcelength, lengthIsQN = reaper.GetMediaSourceLength(reaper.GetMediaItemTake_Source(item_activetake))
+      if lengthIsQN then
+        item_activetake_mediasourcelength = reaper.TimeMap_QNToTime(item_activetake_mediasourcelength)
+      end
+      item_activetake_mediasourcelength = item_activetake_mediasourcelength / reaper.GetMediaItemTakeInfo_Value(item_activetake, "D_PLAYRATE")
+      item = reaper.SplitMediaItem(item, reaper.GetMediaItemInfo_Value(item, "D_POSITION") + item_activetake_mediasourcelength)
+    end
+  until not item
+  return
+end
+local IS_ITEM_LOCKED = {
+  [1.0] = true,
+  [3.0] = true
+}
 local item = {}
-for i=0,c-1 do item[i] = r.GetSelectedMediaItem(0,i) end
-for i=0,c-1 do bfut_SplitLpdItmsIntoSeparateItms(item[i]) end
-r.PreventUIRefresh(-1)
-r.UpdateArrange()
-r.Undo_EndBlock2(0,"bfut_Split looped item into separate items",-1)
+for i = 0, COUNT_SEL_ITEMS - 1 do
+  item[i] = reaper.GetSelectedMediaItem(0, i)
+end
+reaper.PreventUIRefresh(1)
+reaper.Undo_BeginBlock2(0)
+for i = 0, COUNT_SEL_ITEMS - 1 do
+  if not IS_ITEM_LOCKED[reaper.GetMediaItemInfo_Value(item[i], "C_LOCK")] then
+    bfut_SplitLoopedItemAtLoopPoints(item[i])
+  end
+end
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
+reaper.Undo_EndBlock2(0, "bfut_Split looped item into separate items", -1)
