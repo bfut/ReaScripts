f3158e4807a7d21cdeb441b6b99fdbfbbe2dc6dd
bfut <34510704+bfut@users.noreply.github.com>
2020-02-22 13:47:04 +0100

    Change: bfut_Copy items to project markers, remove overlaps.lua

--- Items Editing/bfut_Copy items to project markers, remove overlaps.lua
+++ Items Editing/bfut_Copy items to project markers, remove overlaps.lua
@@ -1,126 +1,116 @@
 --[[
   @author bfut
-  @version 1.0
-  DESCRIPTION: bfut_Copy items to project markers, remove overlaps
-  REQUIRES: Reaper v5.70 or later
+  @version 2.0
+  @description bfut_Copy items to project markers, remove overlaps
+  @about
+    Copies any number of selected items to project markers.
+
+    HOW TO USE:
+      1) There must be at least one project marker.
+      2) Select media item(s).
+      3) Run the script.
+
+    REQUIRES: Reaper v6.04 or later
+  @changelog
+    + native trim behind items behavior
+    + native envelope behavior
+    + inherit item grouping behavior from native copy+paste
+    + improved performance
+  @website https://github.com/bfut
   LICENSE:
-    Copyright (c) 2017 and later Benjamin Futasz <bendfu@gmail.com><https://github.com/bfut>
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+    Copyright (C) 2017 and later Benjamin Futasz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
-local reaper = reaper
+local function bfut_GetMarkersByDistanceToNext(count_markers, count_regions)
+  local markers = {}
+  local marker_idx = 2
+  local continue_loop = 0
+  for i = 0, count_markers + count_regions - 1 do
+    local retval, isregion, pos, _, _, _ = reaper.EnumProjectMarkers2(0, i)
+    continue_loop = i
+    if retval and not isregion then
+      markers[1] = {1, pos, pos}
+      break
+    end
+  end
+  for i = continue_loop + 1, count_markers + count_regions - 1 do
+    local _, isregion, pos, _, _, _ = reaper.EnumProjectMarkers2(0, i)
+    if not isregion then
+      markers[marker_idx-1][3] = pos - markers[marker_idx-1][2]
+      markers[marker_idx] = {marker_idx, pos, pos}
+      marker_idx = marker_idx + 1
+    end
+  end
+  markers[marker_idx-1][3] = 1 / 0
+  table.sort(markers, function(a, b) return a[3] > b[3] end)
+  for i = marker_idx - 1, 1, -1 do
+    if markers[i][3] > 0 then
+      break
+    end
+    markers[i] = nil
+  end
+  return markers
+end
+local function bfut_SetTimeSelection(timesel_start, timesel_len)
+  reaper.Main_OnCommandEx(40635, 0)
+  reaper.MoveEditCursor(timesel_start - reaper.GetCursorPosition(0), false)
+  reaper.MoveEditCursor(timesel_len, true)
+end
+local retval, COUNT_MARKERS, COUNT_REGIONS = reaper.CountProjectMarkers(0)
+if not retval or COUNT_MARKERS < 1 then
+  return
+end
+local FIRST_ITEM = reaper.GetSelectedMediaItem(0, 0)
+if not FIRST_ITEM then
+  return
+end
+reaper.SetOnlyTrackSelected(reaper.GetMediaItem_Track(FIRST_ITEM))
+reaper.Main_OnCommandEx(40914, 0)
+local PLAY_STATE = reaper.GetPlayStateEx(0)
+local PLAY_POSITION = reaper.GetPlayPosition()
+if PLAY_STATE == 1 then
+  reaper.CSurf_OnPause()
+end
+reaper.Main_OnCommandEx(40630, 0)
+local ORIGINAL_TIMESEL_START = reaper.GetCursorPosition(0)
+reaper.Main_OnCommandEx(40631, 0)
+local ORIGINAL_TIMESEL_LEN = reaper.GetCursorPosition(0) - ORIGINAL_TIMESEL_START
+local markers = bfut_GetMarkersByDistanceToNext(COUNT_MARKERS, COUNT_REGIONS)
+reaper.Main_OnCommandEx(41174, 0)
+markers[1][3] = reaper.GetCursorPosition()
+reaper.Main_OnCommandEx(41173, 0)
+markers[1][3] = markers[1][3] - reaper.GetCursorPosition()
+local SEL_ITEMS_LEN = markers[1][3]
 reaper.Undo_BeginBlock2(0)
-local gl_num_items = reaper.CountSelectedMediaItems(0)
-if gl_num_items < 1 then
-  return
-end
-local _, gl_num_markers = reaper.CountProjectMarkers(0)
-if gl_num_markers < 1 then
-  return
-end
 reaper.PreventUIRefresh(1)
-local gl_timesel_start
-local gl_timesel_len
-local gl_envelope_points
-reaper.Main_OnCommandEx(40630,0)
-gl_timesel_start = reaper.GetCursorPosition(0)
-reaper.Main_OnCommandEx(40631,0)
-gl_timesel_len = reaper.GetCursorPosition(0)-gl_timesel_start
-gl_envelope_points = reaper.GetToggleCommandState(40070)
-if gl_envelope_points == 1 then
-  reaper.Main_OnCommandEx(40070,0)
+reaper.SetCursorContext(1, nil)
+for _, val in ipairs(markers) do
+  reaper.Main_OnCommandEx(41173, 0)
+  bfut_SetTimeSelection(reaper.GetCursorPosition(), math.min(val[3], SEL_ITEMS_LEN))
+  reaper.GoToMarker(0, val[1], true)
+  reaper.Main_OnCommandEx(41383, 0)
+  reaper.Main_OnCommandEx(40058, 0)
 end
-reaper.GoToMarker(0,1,true)
-reaper.Main_OnCommandEx(40698,0)
-reaper.Main_OnCommandEx(40058,0)
-reaper.Main_OnCommandEx(40626,0)
-local gl_num_grouped_items = 0
-local gl_grouping={}
-for j=0,gl_num_items-1 do
-  local groupid = reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID")
-  if groupid > 0.0 then
-    gl_grouping[j] = groupid
-    gl_num_grouped_items = gl_num_grouped_items+1
-  else
-    reaper.SetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID",-1)
-  end
-end
-local function PasteItemsWithGroups(gl_num_grouped_items)
-  for i=2,gl_num_markers do
-    reaper.GoToMarker(0,i,true)
-    reaper.Main_OnCommandEx(40625,0)
-    reaper.Main_OnCommandEx(40312,0)
-    reaper.Main_OnCommandEx(40058,0)
-    reaper.Main_OnCommandEx(40626,0)
-    for j=0,gl_num_items-1 do
-      if gl_grouping[j] == nil then
-        reaper.SetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID",-1)
-      else
-        reaper.SetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID",gl_grouping[j])
-      end
-    end
-  end
-  gl_grouping = nil
-  reaper.Main_OnCommandEx(40034,0)
-  for j=0,reaper.CountSelectedMediaItems(0)-1 do
-    if reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID") == -1 then
-      reaper.SetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,j),"I_GROUPID",0.0)
-    end
-  end
-end
-local function PasteItemsNoGroups()
-  reaper.Main_OnCommandEx(40033,0)
-  reaper.Main_OnCommandEx(40032,0)
-  local gl_pasted_items_start
-  local gl_first_item
-  reaper.Main_OnCommandEx(41173,0)
-  gl_pasted_items_start = reaper.GetCursorPosition(0)
-  local k=0
-  while reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0,k), "D_POSITION")-gl_pasted_items_start > 0 do
-    k=k+1
-  end
-  gl_first_item = reaper.GetSelectedMediaItem(0,k)
-  for i=2,gl_num_markers do
-    reaper.GoToMarker(0,i,true)
-    reaper.Main_OnCommandEx(40625,0)
-    reaper.Main_OnCommandEx(40312,0)
-    reaper.Main_OnCommandEx(40058,0)
-    reaper.Main_OnCommandEx(40626,0)
-    reaper.SetMediaItemSelected(gl_first_item, true)
-    reaper.Main_OnCommandEx(40034,0)
-    reaper.Main_OnCommandEx(40032,0)
-  end
-  reaper.Main_OnCommandEx(40033,0)
-end
-if gl_num_grouped_items > 0 then
-  PasteItemsWithGroups(gl_num_grouped_items)
+bfut_SetTimeSelection(ORIGINAL_TIMESEL_START, ORIGINAL_TIMESEL_LEN)
+reaper.MoveEditCursor(PLAY_POSITION - reaper.GetCursorPosition(0), false)
+if PLAY_STATE then
+  reaper.CSurf_OnPause()
 else
-  gl_grouping = nil
-  PasteItemsNoGroups()
+  reaper.CSurf_GoEnd()
 end
-reaper.Main_OnCommandEx(40635,0)
-reaper.MoveEditCursor(gl_timesel_start-reaper.GetCursorPosition(0), false)
-reaper.MoveEditCursor(gl_timesel_len, true)
-reaper.Main_OnCommandEx(41174,0)
-if gl_envelope_points == 1 then
-  reaper.Main_OnCommandEx(40070,0)
-end
+reaper.Main_OnCommandEx(41174, 0)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
-reaper.Undo_EndBlock2(0,"bfut_Copy items to project markers, remove overlaps",-1)
+reaper.Undo_EndBlock2(0, "bfut_Copy items to project markers, remove overlaps", -1)
