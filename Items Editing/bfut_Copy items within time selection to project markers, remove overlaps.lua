566f070bc827cf0bc53ec362ad81389bc7d34e5c
bfut <34510704+bfut@users.noreply.github.com>
2022-11-19 15:59:35 +0100

    Update: bfut_Copy items to project markers (propagate)

--- Items Editing/bfut_Copy items within time selection to project markers, remove overlaps.lua
+++ Items Editing/bfut_Copy items within time selection to project markers, remove overlaps.lua
@@ -1,6 +1,6 @@
 --[[
   @author bfut
-  @version 2.2
+  @version 2.3
   @description bfut_Copy items within time selection to project markers, remove overlaps
   @about
     Propagates selected items within time selection to project markers.
@@ -11,11 +11,9 @@
       3) Set time selection that (partially) overlaps selected item(s).
       4) Run the script.
 
-    REQUIRES: Reaper v6.69 or later
+    REQUIRES: Reaper v6.70 or later
   @changelog
-    + ignore markers with distance to next below time range length threshold
-    + Fix: strictly paste items to area between markers
-    + Fix: paste items to respective original tracks
+    + support time signature markers
   @website https://github.com/bfut
   LICENSE:
     Copyright (C) 2017 and later Benjamin Futasz
@@ -39,48 +37,49 @@
 local CONFIG = {
   items_or_sel = "sel"
 }
-local function bfut_GetMarkersByDistanceToNext(count_markers, count_regions)
+local function bfut_GetMarkersByDistanceToNext(count_markers, count_regions, min_range_len)
   local markers = {}
   local marker_idx = 2
   local continue_loop = 0
-  local min_note_len = 4.2615384614919 * 10^-5
   for i = 0, count_markers + count_regions - 1 do
     local retval, isregion, pos, _, _, _ = reaper.EnumProjectMarkers2(0, i)
     continue_loop = i
     if retval and not isregion then
-      markers[1] = {1, pos, pos}
+      markers[1] = {1, pos, nil, nil}
       break
     end
   end
   for i = continue_loop + 1, count_markers + count_regions - 1 do
     local _, isregion, pos, _, _, _ = reaper.EnumProjectMarkers2(0, i)
     if not isregion then
-      markers[marker_idx-1][3] = pos - markers[marker_idx-1][2]
-      markers[marker_idx] = {marker_idx, pos, pos}
+      markers[marker_idx - 1][3] = pos - markers[marker_idx - 1][2]
+      markers[marker_idx - 1][4] = reaper.TimeMap2_timeToQN(0, pos) - reaper.TimeMap2_timeToQN(0, markers[marker_idx - 1][2])
+      markers[marker_idx] = {marker_idx, pos, nil, nil}
       marker_idx = marker_idx + 1
     end
   end
-  markers[marker_idx-1][3] = 1 / 0
+  markers[marker_idx - 1][3] = 1 / 0
+  markers[marker_idx - 1][4] = 1 / 0
   table.sort(markers, function(a, b) return a[3] > b[3] end)
   for i = marker_idx - 1, 1, -1 do
-    if markers[i][3] >= min_note_len then
+    if markers[i][3] >= min_range_len then
+      table.sort(markers, function(a, b) return a[4] > b[4] end)
       return markers, i
     end
     markers[i] = nil
   end
 end
-local function bfut_UnselectItemsOutsideSelectionRange(range_begin, range_end)
+local function bfut_UnselectItemsOutsideTimeSelection(range_begin, range_end, min_range_len)
   local count_sel_items = reaper.CountSelectedMediaItems(0)
   for i = count_sel_items - 1, 0, -1 do
     local item = reaper.GetSelectedMediaItem(0, i)
-    if item then
-      local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-      if pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") <= range_begin or pos >= range_end then
-        if count_sel_items == 1 then
-          return false
-        end
-        reaper.SetMediaItemSelected(item, false)
+    local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    if pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") - range_begin < min_range_len or
+        range_end - pos < min_range_len then
+      if count_sel_items == 1 then
+        return false
       end
+      reaper.SetMediaItemSelected(item, false)
     end
   end
   if reaper.CountSelectedMediaItems(0) < 1 then
@@ -95,16 +94,16 @@ end
 if not reaper.GetSelectedMediaItem(0, 0) then
   return
 end
+local MIN_RANGE_LEN = 4.2615384614919 * 10^-5
 local ORIGINAL_TIMESEL_START, ORIGINAL_TIMESEL_END = reaper.GetSet_LoopTimeRange2(0, false, false, -1, -1, false)
 if CONFIG["items_or_sel"] ~= "items" then
   local count_sel_items = reaper.CountSelectedMediaItems(0)
   for i = count_sel_items - 1, 0, -1 do
     local item = reaper.GetSelectedMediaItem(0, i)
-    if item then
-      local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-      if pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") <= ORIGINAL_TIMESEL_START or pos >= ORIGINAL_TIMESEL_END then
-        count_sel_items = count_sel_items - 1
-      end
+    local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    if pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") - ORIGINAL_TIMESEL_START < MIN_RANGE_LEN or
+        ORIGINAL_TIMESEL_END - pos < MIN_RANGE_LEN then
+      count_sel_items = count_sel_items - 1
     end
   end
   if count_sel_items < 1 then
@@ -121,33 +120,31 @@ end
 reaper.Undo_BeginBlock2(0)
 reaper.PreventUIRefresh(1)
 if CONFIG["items_or_sel"] ~= "items" then
-  for i = reaper.CountSelectedMediaItems(0) - 1, 0, -1 do
-    local item = reaper.GetSelectedMediaItem(0, i)
-    if item then
-      local pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-      if pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH") <= ORIGINAL_TIMESEL_START or pos >= ORIGINAL_TIMESEL_END then
-        reaper.SetMediaItemSelected(item, false)
-      end
-    end
-  end
+  bfut_UnselectItemsOutsideTimeSelection(ORIGINAL_TIMESEL_START, ORIGINAL_TIMESEL_END, MIN_RANGE_LEN)
 end
-local markers, count_target_markers = bfut_GetMarkersByDistanceToNext(COUNT_MARKERS, COUNT_REGIONS)
+local markers, count_target_markers = bfut_GetMarkersByDistanceToNext(COUNT_MARKERS, COUNT_REGIONS, MIN_RANGE_LEN)
 if CONFIG["items_or_sel"] == "items" then
   reaper.Main_OnCommandEx(41174, 0)
   markers[1][3] = reaper.GetCursorPosition()
+  markers[1][4] = reaper.TimeMap2_timeToQN(0, markers[1][3])
   reaper.Main_OnCommandEx(41173, 0)
-  markers[1][3] = markers[1][3] - reaper.GetCursorPosition()
+  local cpos = reaper.GetCursorPosition()
+  markers[1][3] = markers[1][3] - cpos
+  markers[1][4] = markers[1][4] - reaper.TimeMap2_timeToQN(0, cpos)
 else
   markers[1][3] = ORIGINAL_TIMESEL_END - ORIGINAL_TIMESEL_START
+  markers[1][4] = reaper.TimeMap2_timeToQN(0, ORIGINAL_TIMESEL_END) - reaper.TimeMap2_timeToQN(0, ORIGINAL_TIMESEL_START)
 end
 local MAX_LEN = markers[1][3]
+local MAX_LEN_QN = markers[1][4]
 reaper.SetCursorContext(1, nil)
 if CONFIG["items_or_sel"] == "items" then
   for _, val in ipairs(markers) do
     reaper.Main_OnCommandEx(41173, 0)
     local pos = reaper.GetCursorPosition()
-    local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, pos, pos + math.min(val[3], MAX_LEN), false)
-    bfut_UnselectItemsOutsideSelectionRange(curr_timesel_start, curr_timesel_end)
+    local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, pos,
+      reaper.TimeMap2_QNToTime(0, reaper.TimeMap2_timeToQN(0, pos) + math.min(val[4], MAX_LEN_QN)), false)
+    bfut_UnselectItemsOutsideTimeSelection(curr_timesel_start, curr_timesel_end, MIN_RANGE_LEN)
     reaper.SetOnlyTrackSelected(reaper.GetMediaItem_Track(reaper.GetSelectedMediaItem(0, 0)))
     reaper.Main_OnCommandEx(40914, 0)
     reaper.SetEditCurPos2(0, val[2], false, false)
@@ -163,10 +160,12 @@ else
     reaper.Main_OnCommandEx(41383, 0)
     reaper.SetEditCurPos2(0, markers[i][2], false, false)
     reaper.Main_OnCommandEx(40058, 0)
-    local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, markers[i][2], markers[i][2] + math.min(markers[i + 1][3], MAX_LEN), false)
-    item_left = bfut_UnselectItemsOutsideSelectionRange(curr_timesel_start, curr_timesel_end)
+    local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, markers[i][2],
+      reaper.TimeMap2_QNToTime(0, reaper.TimeMap2_timeToQN(0, markers[i][2]) + math.min(markers[i + 1][4], MAX_LEN_QN)), false)
+    item_left = bfut_UnselectItemsOutsideTimeSelection(curr_timesel_start, curr_timesel_end, MIN_RANGE_LEN)
     if not item_left then
-      local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, markers[i][2], markers[i][2] + math.min(markers[i + 0][3], MAX_LEN), false)
+      local curr_timesel_start, curr_timesel_end = reaper.GetSet_LoopTimeRange2(0, true, false, markers[i][2],
+        reaper.TimeMap2_QNToTime(0, reaper.TimeMap2_timeToQN(0, markers[i][2]) + math.min(markers[i + 0][4], MAX_LEN_QN)), false)
       break
     end
     reaper.SetOnlyTrackSelected(reaper.GetMediaItem_Track(reaper.GetSelectedMediaItem(0, 0)))
